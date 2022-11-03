aa808c21b5df3cdfc2b766a9c406417e1dc894c7
Tamara Rodrigues <99697093+tamara-w@users.noreply.github.com>
2022-11-02 23:08:49 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+let isIgnite = false
+function changeCard(event) {
+  const card = event.currentTarget
+  const bg = isIgnite ? "explore" : "ignite"
+  isIgnite = !isIgnite
+  card.style.backgroundImage = `url(./assets/bg-${bg}.svg)`
+}
